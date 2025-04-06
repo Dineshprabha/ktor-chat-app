9f363475fa5b4d94cd1870622d6135fc5a4fe2f3
Dinesh N <dinesh.prabha1205@gmail.com>
2025-04-06 15:11:44 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "com.dinesh"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+
+    implementation(libs.ktor.server.resources)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.request.validation)
+
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+
+    implementation(libs.ktor.server.websockets)
+}
